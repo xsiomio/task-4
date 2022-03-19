22937583e3ba010aca87a4c708c62f158deeedb9
Assem <Assem@DESKTOP-AOSJQJN>
2022-03-19 21:59:34 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string name;
+        string zodiac;
+        string age;
+        string work;
+
+        Console.WriteLine("Как Вас зовут?");
+        name = Console.ReadLine();
+        Console.WriteLine("Ваш знак зодиака?");
+        zodiac = Console.ReadLine();
+        Console.WriteLine("Сколько Вам лет?");
+        age = Console.ReadLine();
+        Console.WriteLine("Где вы работаете?");
+        work = Console.ReadLine();
+
+        Console.WriteLine("Вас зовут " + name + "," + "Вам " + age + " вы " + "," + zodiac + " и работаете на "+work);
+    }
+}
